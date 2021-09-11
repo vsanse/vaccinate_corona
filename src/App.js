883b0c69f3369768ab880b207e954b5b9bdd47ce
Vishal Sanserwal <vsanse@outlook.com>
2021-09-11 21:15:04 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import item from "./item.png";
+import "./App.css";
+import { useRef, useState } from "react";
 
+const WRAPPER__SIZE = 250;
+const innerHeight = window.innerHeight - WRAPPER__SIZE;
+const innerWidth = window.innerHeight - WRAPPER__SIZE;
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 function App() {
+  const phobicRef = useRef(null);
+  const [caughtTimes, setCaugthTimes] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  // let prevTop = 0;
+  // let prevLeft = 0;
+
+  const handlePositionUpdate = (top, left) => {
+    // console.log(`top, left`, top, left);
+    phobicRef.current.style.top = `${top}px`;
+    phobicRef.current.style.left = `${left}px`;
+  };
+
+  const regenerateItem = () => {
+    phobicRef.current.style.height = "0px";
+    phobicRef.current.style.width = "0px";
+    setTimeout(() => {
+      phobicRef.current.style.height = `${WRAPPER__SIZE}px`;
+      phobicRef.current.style.width =  `${WRAPPER__SIZE}px`;
+      phobicRef.current.style.top = `${randomIntFromInterval(0, innerHeight)}px`;
+      phobicRef.current.style.left = `${randomIntFromInterval(0, innerWidth)}px`;
+    }, 1500);
+  };
+
+  const handleMouseEnter = (evt) => {
+    const mouseTop = evt.clientX;
+    const mouseLeft = evt.clientY;
+    const randomTop = randomIntFromInterval(0, innerHeight);
+    const randomLeft = randomIntFromInterval(0, innerWidth);
+    if (
+      mouseTop >= randomTop &&
+      mouseTop <= randomTop + WRAPPER__SIZE &&
+      mouseLeft >= randomLeft &&
+      mouseLeft <= randomLeft + WRAPPER__SIZE
+    ) {
+      setCaugthTimes((prev) => prev + 1);
+      setGameOver(true);
+      regenerateItem();
+      setTimeout(() => {
+        setGameOver(false);
+      }, 1000);
+    } else {
+      handlePositionUpdate(randomTop, randomLeft);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App__score">Killed {caughtTimes} Times</p>
+      <div
+        className="App__phobicWrapper"
+        ref={phobicRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseOver={handleMouseEnter}
+      >
+        <img src={item} className="App-logo App__phobic--img" alt="logo" />
+      </div>
+      {gameOver && (
+        <div className="App__GameOver">
+          <p className="title"> 😈 Corona Killed 🔪 Score: {caughtTimes}</p>
+        </div>
+      )}
     </div>
   );
 }
